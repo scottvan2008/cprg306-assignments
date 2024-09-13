e234b82f9f3637a26ee736aa2b4c06944963edd5
Scott Yinan Fan <scottvan2008@gmail.com>
2024-09-14 04:13:54 +0800

    Week 2 assignment done
    1. Root page
    2. Week 2 page
    3. Component page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,15 @@
+import Link from "next/link";
+import StudentInfo from "./week-2/student-info";
 
 export default function Home() {
   return (
     <main>
-
+      <h1 className="text-4xl">CPRG 306: Web Development 2 - Assignments</h1>
+      <ul>
+        <li>
+          <Link href="./week-2">Week 2</Link>
+        </li>
+      </ul>
 
     </main>
   );
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return(
+        <div>
+            <p>Scott Yinan Fan</p>
+            <Link href="https://github.com/scottvan2008/cprg306-assignments">https://github.com</Link>
+        </div>
+    )
+}
